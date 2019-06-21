2ca5287ba36f63a19365fd3646135d9591bfadf8
jonathan <Jalbr03droid@gmail.com>
2019-06-21 10:21:17 -0600

    added the function add_up

--- add-up-the-numbers-from-a-single-number/src/main.py
+++ add-up-the-numbers-from-a-single-number/src/main.py
@@ -1,7 +1,6 @@
 #vars-
 done = False
 num = 0
-total = 0
 numeric = True;
 
 def is_number(n):
@@ -11,6 +10,15 @@ def is_number(n):
     except ValueError:
         return False
 
+def add_up(n):
+    total = 0
+    n = int(num)
+    for i in range(0, n + 1):
+        total += i
+    print(total)
+
+
+
 while not done:
     total = 0
     num = input("type a number or type quit to end the program ")
@@ -19,9 +27,5 @@ while not done:
         if(num.capitalize() == "Quit"):
             done = True
 
-    else:
-        num = int(num)
-        for i in range(0,num+1):
-            total += i
-        print(total)
+    add_up(num)
 
