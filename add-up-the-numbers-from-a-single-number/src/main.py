8ff9e28b316c1da54eeeb62cf1b8e98107f70f94
jonathan <Jalbr03droid@gmail.com>
2019-06-21 09:35:37 -0600

    DONE!

---
+++ add-up-the-numbers-from-a-single-number/src/main.py
@@ -0,0 +1,26 @@
+#vars-
+done = False
+num = 0
+total = 0
+numeric = True;
+
+def is_number(n):
+    try:
+        float(n)
+        return True
+    except ValueError:
+        return False
+
+while not done:
+    num = input("type a number or type quit to end the program ")
+
+    if(is_number(num) == False):
+        if(num.capitalize() == "Quit"):
+            done = True
+
+    else:
+        num = int(num)
+        for i in range(0,num+1):
+            total += i
+        print(total)
+
